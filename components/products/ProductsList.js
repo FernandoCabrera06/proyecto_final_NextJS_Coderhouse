2c4aae673f51089726570611ebeca1d845a9cede
Fernando Damián Cabrera <fernando_cabrera2006@hotmail.com>
2024-09-10 01:52:17 -0300

    logs para ver errores

--- components/products/ProductsList.js
+++ components/products/ProductsList.js
@@ -1,21 +1,32 @@
 import ProductCard from "./ProductCard"
 
 const ProductList = async ({ category }) => {
-  //cache configurado, para no cachear nunca y la informacion siempre este actulizada.
-  let items = []
+  let items = [];
 
-  const apiUrl = `https://${process.env.VERCEL_URL}/api/productos/${category}`;
   try {
-console.log("Fetching from URL:", apiUrl);
+    const apiUrl = `https://${process.env.VERCEL_URL}/api/productos/${category}`;
+    console.log("Fetching from URL:", apiUrl);
 
-     items = await fetch(apiUrl, {
-        cache: 'no-cache',
-    }).then(r => r.json())
-    console.log("PRODUCT LIST")
+    const response = await fetch(apiUrl, {
+      cache: 'no-cache',
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("application/json")) {
+      const text = await response.text(); // Leer la respuesta como texto
+      console.error(`Expected JSON response, but got ${contentType}: ${text}`);
+      return; // O lanzar un error
+    }
+
+    items = await response.json();
+    console.log("PRODUCT LIST");
     console.log(items);
   } catch (e) {
-    console.log("Hubo un error al traer los datos: " + e)
-    console.log("Fetching from URL:", apiUrl);
+    console.error("Hubo un error al traer los datos: " + e.message);
   }
 
   return (
@@ -25,7 +36,7 @@ console.log("Fetching from URL:", apiUrl);
         <ProductCard key={item.id} item={item} />
       ))}
     </section>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
